fef4af247e69b65ddaf5bfee00eedaf8a7f36a6d
Ayodeji97 <danielayodeji97@gmail.com>
2022-09-24 14:27:12 +0100

    initial commit

---
+++ app/src/main/java/com/danzucker/ocadoproducts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danzucker.ocadoproducts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
